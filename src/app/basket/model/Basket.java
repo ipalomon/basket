579fb5670c68239f4267d5caa46621b26619d30f
Alumne_mati1 <ipalomon@gmail.com>
2022-05-03 13:02:28 +0200

    commit #7 Version 0.1 Class basket

--- src/app/basket/model/Basket.java
+++ src/app/basket/model/Basket.java
@@ -1,4 +1,42 @@
 package app.basket.model;
 
+import java.util.HashMap;
+
+import app.product.model.Product;
+
 public class Basket {
+    private int basketId;
+    private int userId;
+    // product, productQuantity
+    private HashMap<Product, Double> productsListBasket;
+
+    public Basket(int basketId, int userId, HashMap<Product, Double> productsListBasket) {
+        this.basketId = basketId;
+        this.userId = userId;
+        this.productsListBasket = new HashMap<>();
+    }
+
+    public int getBasketId() {
+        return this.basketId;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public HashMap<Product, Double> getProductsListBasket() {
+        return this.productsListBasket;
+    }
+
+    public void setBasketId(int basketId) {
+        this.basketId = basketId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void addProductToBasket(Product product, Double productQuantity) {
+        this.productsListBasket.put(product, productQuantity);
+    }
 }
